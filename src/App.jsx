6a5fd211036c144df81ca628a6c2645175e51afa
Afrillian Ramadhan <pingski.ramadhan@gmail.com>
2022-08-18 15:27:43 +0700

    with-sidebar & routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   );
 }
 
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,10 +1,47 @@
 import React from "react";
-import { Button, Stack, VStack } from "@chakra-ui/react";
+import { Button, VStack, Flex, Text, Image } from "@chakra-ui/react";
+import logo from "../assets/logo-kementan.png";
+import { FiHome } from "react-icons/fi";
 
 const SideBar = () => {
   return (
-    <VStack>
-      <Box bgColor={"gray.200"}>Halo</Box>
+    <VStack
+      spacing={3}
+      bgColor={"green.500"}
+      w={"100%"}
+      maxW={"15%"}
+      h={"100vh"}
+      textColor={"white"}
+      justifyContent={"flex-start"}
+      py={"0.5em"}
+      pos={"sticky"}
+      left={0}
+      px={"0.5em"}
+    >
+      <Flex>
+        <Image src={logo} boxSize={"60px"} />
+      </Flex>
+      <Flex>
+        <Text fontFamily={"poppins"} fontWeight={"bold"}>
+          Dashboard
+        </Text>
+      </Flex>
+      <Flex
+        justifyContent={"space-evenly"}
+        alignItems={"center"}
+        cursor={"pointer"}
+        _hover={{
+          bg: "whiteAlpha.800",
+          color: "black",
+        }}
+        p={4}
+        mx={4}
+        borderRadius={"md"}
+        w={"80%"}
+      >
+        <FiHome />
+        Home
+      </Flex>
     </VStack>
   );
 };
